5bbde191191408df9bb9e312d6f6f489258656af
Microsoft <Microsoft@ALSHAIMAA>
2025-08-08 12:13:59 +0300

    Example01

---
+++ Demo/Car.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Car
+    {
+        #region Attributes
+
+        private int id;
+
+        private string? model;
+
+        private int speed;
+
+
+
+
+
+
+        #endregion
+
+        #region Properties
+
+         public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string? Model
+        {
+            get { return model; }
+            set { model = value; }
+        }   
+        public int Speed
+        {
+            get { return speed; }
+            set { speed = value; }
+        }   
+
+
+
+        #endregion
+
+
+        #region Constructor
+        public Car(int id, string? model, int speed)
+        {
+            this.id = id;
+            this.model = model;
+            this.speed = speed;
+        }
+
+
+
+        #endregion
+        #region Fuction
+
+        public override string ToString()
+        {
+            return $"Car ID: {id}, Model: {model}, Speed: {speed} km/h";    
+        }
+
+        #endregion
+
+
+
+
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region Example 01
+
+
+            //Car c1;
+
+            //// Create a new instance of the Car class
+            //// reeference refer to Default value(null)
+            //// c1 reference refer to instance from type Car or any type class inherrstence from Car 
+            //// cle initiliaze 4 Byte in stack for c1
+
+            //c1 = new Car( 5 ,"BM" ,250); // = c1 = new(5 ,"toyota" ,220) Syntax Suger
+            //// c1 reference refer to instance from type Car or any type class inherrstence from Car
+            //// allocate (4 , 8 , 4)  16 bytes in heap for c1    
+            //// initialize the attributes of the Car instance with Default values(0,"null ,0)
+            //// call the constructor of the Car class with parameters (5, "BM", 250)
+
+            ////Assigne address from instance to reference ,
+
+            //Console.WriteLine(c1);
+
+            #endregion
+
+
+
         }
     }
 }
